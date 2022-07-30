f9f303fe1597a9be461cfcd1a676572f3c4285af
Daniel <phwelo@gmail.com>
2022-07-30 02:10:59 -0400

    fix sp in comment ;P

--- DevOps/state-metrics.tf
+++ DevOps/state-metrics.tf
@@ -198,7 +198,7 @@ resource "kubernetes_deployment" "state_metrics_local" {
         }
 
         annotations = {
-          # Full DD integradion doc:
+          # Full DD integration doc:
           # https://github.com/DataDog/integrations-core/blob/master/kubernetes_state/datadog_checks/kubernetes_state/data/conf.yaml.example
           "ad.datadoghq.com/${var.app_name}.check_names"  = jsonencode(["kubernetes_state"])
           "ad.datadoghq.com/${var.app_name}.init_configs" = "[{}]"
