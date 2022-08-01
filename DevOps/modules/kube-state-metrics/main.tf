3c21a859baeaac38ac3c4f78555ef97b08de52c2
Daniel <phwelo@gmail.com>
2022-08-01 18:21:57 -0400

    begin buildout of kube-state-metrics module

--- DevOps/modules/kube-state-metrics/main.tf
+++ DevOps/modules/kube-state-metrics/main.tf
@@ -1,6 +1,19 @@
 module "single_container_app" {
-  source = "./modules/single_container_deployment"
-  providers = {
-    kubernetes = kubernetes.local
-  }
+  source = "../single_container_deployment"
+  kubeconfig_path = var.kubeconfig_path
+  namespace_name = var.namespace_name
+  app_name = var.app_name
+  docker_image = var.docker_image
+  health_check = var.health_check
+  host_networking = var.host_networking
+  docker_cmd = var.docker_cmd
+  limits = var.limits
+  requests = var.requests
+  replicas = var.replicas
+  deployment_timeout = var.deployment_timeout
+  deployment_strategy = var.deployment_strategy
+  cluster_role_rules = var.cluster_role_rules
+  dd_config = var.dd_config
+  
+
 }
